3818b34a65e59eb470b112c50433c998a6e32168
Lopo <lopo@losys.eu>
2016-02-02 11:01:40 +0100

    fix sql column size

--- sql/000-init.sql
+++ sql/000-init.sql
@@ -6,7 +6,7 @@ DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
-  `password` varchar(32) NOT NULL,
+  `password` varchar(60) NOT NULL,
   `level` smallint(5) unsigned NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
