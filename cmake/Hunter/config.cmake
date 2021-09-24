85ccc3bd8ddd901ec721bb0ade1065d87cff1bf3
Lopo <lopo@losys.eu>
2021-09-24 18:31:49 +0200

    update boost

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,5 @@
 message("Loading Hunter config.cmake")
-#hunter_config(Boost
-#	VERSION ${HUNTER_Boost_VERSION}
-#	CMAKE_ARGS USE_CONFIG_FROM_BOOST=ON
-#	)
-hunter_config(Boost VERSION 1.56.0)
+hunter_config(Boost
+	VERSION ${HUNTER_Boost_VERSION}
+	CMAKE_ARGS USE_CONFIG_FROM_BOOST=ON
+	)
